d2bd6de701d4012e31b95faecf3148dd2489391a
JunhiKim <tjsej1324@gmail.com>
2021-02-09 23:33:39 +0900

    정렬 - 완 -

--- src/BAEKJOON/S2751.java
+++ src/BAEKJOON/S2751.java
@@ -15,26 +15,46 @@ package BAEKJOON;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class S2751 {
-
+	int index = 0, min;
+	static int n;
+	static int temp;
+	static int[] array;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int index = 0, min, temp;
-		StringBuilder sb = new StringBuilder();
-		int n = Integer.parseInt(br.readLine());
 		
-		int[] array = new int[n];
+		StringBuilder sb = new StringBuilder();
+		n= Integer.parseInt(br.readLine());
+		
+		//array = new int[n];
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for(int i = 0; i < n; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(list);
+		
+		for(int value : list) {
+			sb.append(value).append('\n');
+		}
+		System.out.println(sb);
+		/*
 		for(int i = 0; i < n; i++) {
 			array[i] = Integer.parseInt(br.readLine());
 		}
-		
+		*/
 		/*
-		// 선택정렬
+		// 선택정렬 N^2
 
 		for (int i = 0; i < n; i++) {
 			min = Integer.MAX_VALUE;
@@ -53,7 +73,8 @@ public class S2751 {
 		System.out.println(sb);
 		*/
 		
-		//버블정렬
+		//버블정렬 N^2
+		/*
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n-1-i; j++) {
 				if(array[j] > array[j+1]){
@@ -66,8 +87,65 @@ public class S2751 {
 		for(int i = 0; i < n; i++) {
 			System.out.println(array[i]);
 		}
+		*/
+		
+		// 삽입정렬 N^2
+		/*
+		for(int i = 1; i < n; i++) {
+			for(int j = i; j >= 1; j--) {
+				if(array[j]<array[j-1]) {
+					temp = array[j];
+					array[j] = array[j-1];
+					array[j-1] = temp;
+				}else break;
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println(array[i]);
+		}
+		*/
+		
+		//퀵정렬 - 제일 빠르다~~~~ (N * logN)
+		/*
+		quickSort(0, n-1);
+		for(int i = 0; i < n; i++) {
+			System.out.println(array[i]);
+		}
+		*/
+		
+		
 		
 	}
 	
-
+	public static void quickSort(int start, int end) {
+		if(start >= end) return;
+		
+		int key = start;
+		int first = start+1;
+		int last = end;
+		
+		while(first <= last) {
+			while(array[key] >= array[first] && first < end) {
+				first ++;
+			}
+			while(array[last] > array[key] && last > key) {
+				last --;
+			}
+			if(first < last) {
+				temp = array[first];
+				array[first] = array[last];
+				array[last] = temp;
+			}
+			else {
+				temp = array[key];
+				array[key] = array[last];
+				array[last] = temp;
+			}
+		}
+		
+		quickSort(0, last-1);
+		quickSort(last+1, end);
+		
+	}
 }
